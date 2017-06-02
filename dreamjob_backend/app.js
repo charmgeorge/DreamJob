f0cfe80b4f80345b6642d36c2285984a7fe67985
learn <nick.bouldien@gmail.com>
2017-06-02 11:51:52 -0700

    successfully test and add user to database via postman

--- dreamjob_backend/app.js
+++ dreamjob_backend/app.js
@@ -12,6 +12,18 @@ app.get('/', function (request, response) {
   response.json({message: 'API Example App'})
 });
 
+app.post('/create_user', function(request, response){
+  let userParams = request.body.user
+  User.create(userParams).then((user) => {
+    response.status(200)
+    response.json({status:'success', user: user})
+  })
+  .catch((error)=>{
+    response.status(400)
+    response.json({status: 'error', error: error})
+  })
+})
+
 app.listen(4000, function () {
  console.log('Todo Server listening on port 4000!');
 });
