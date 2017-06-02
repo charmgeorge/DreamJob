da249be02bb4acf40d21bc6bc96a5ec73f3982c5
learn <learn@learn01.local>
2017-06-02 10:33:52 -0700

    form and fields completed

--- dreamjob_frontend/src/components/addJob.js
+++ dreamjob_frontend/src/components/addJob.js
@@ -3,6 +3,20 @@ import logo from '../logo.svg';
 import '../App.css';
 
 class addJob extends Component {
+  constructor(props){
+  super(props)
+  this.state={
+    job: {
+      company:"",
+      jobTitle:"",
+      city:"",
+      status:"",
+      date:"",
+      url:"",
+      notes:""
+    }
+  }
+}
   render() {
     return (
       <div className="App">
@@ -10,37 +24,43 @@ class addJob extends Component {
         <form className="form">
           <div className="form-group">
             <label>Company</label>
-            <input />
+            <input type='text' name='company' value={this.state.job.company}/>
             <br />
           </div>
           <div className="form-group">
             <label>Job Title</label>
-            <input />
+            <input type='text' name='jobTitle' value={this.state.job.jobTitle}/>
             <br />
           </div>
           <div className="form-group">
             <label>City</label>
-            <input />
+            <input type='text' name='city' value={this.state.job.city}/>
             <br />
           </div>
           <div className="form-group">
             <label>Status</label>
-            <input />
+            <select name='status' value={this.state.job.status}>
+              <option></option>
+              <option>Interested</option>
+              <option>Applied</option>
+              <option>Interviewed</option>
+              <option>Offered</option>
+            </select>
             <br />
           </div>
           <div className="form-group">
             <label>Date</label>
-            <input />
+            <input type='text' name='date' value={this.state.job.date}/>
             <br />
           </div>
           <div className="form-group">
             <label>Job Posting URL</label>
-            <input />
+            <input type='text' name='url' value={this.state.job.url}/>
             <br />
           </div>
           <div className="form-group">
             <label>Notes</label>
-            <input />
+            <input type='textarea' name='notes' value={this.state.job.notes}/>
             <br />
           </div>
           <div className="form-group">
