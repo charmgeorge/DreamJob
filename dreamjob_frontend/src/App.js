f140bd281aef3fca02cc180d98a3addf33910ad2
learn <learn@learn01.local>
2017-06-02 10:10:37 -0700

    added form input fields

--- dreamjob_frontend/src/App.js
+++ dreamjob_frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import addJob from './components/addJob'
 
 class App extends Component {
   render() {
@@ -8,11 +10,13 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Dream Job</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <Route exact path = '/addJob' component={addJob}></Route>
+          </div>
+        </Router>
       </div>
     );
   }
--- dreamjob_frontend/src/components/addJob.js
+++ dreamjob_frontend/src/components/addJob.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 
 class addJob extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          ADD JOB STARTS HERE
-        </p>
+        <h3>Add A Job</h3>
+        <form className="form">
+          <div className="form-group">
+            <label>Company</label>
+            <input />
+            <br />
+          </div>
+          <div className="form-group">
+            <label>Job Title</label>
+            <input />
+            <br />
+          </div>
+          <div className="form-group">
+            <label>City</label>
+            <input />
+            <br />
+          </div>
+          <div className="form-group">
+            <label>Status</label>
+            <input />
+            <br />
+          </div>
+          <div className="form-group">
+            <label>Date</label>
+            <input />
+            <br />
+          </div>
+          <div className="form-group">
+            <label>Job Posting URL</label>
+            <input />
+            <br />
+          </div>
+          <div className="form-group">
+            <label>Notes</label>
+            <input />
+            <br />
+          </div>
+          <div className="form-group">
+            <input type='submit' value='Submit' />
+            <br />
+          </div>
+        </form>
       </div>
     );
   }
