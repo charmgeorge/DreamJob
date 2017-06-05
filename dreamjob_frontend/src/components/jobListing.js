2c15d6eae2ac9ac906f91e2e0de9f5ad268aadb0
learn <learn@learn01.local>
2017-06-05 11:22:49 -0700

    added job details button to job index page.

--- dreamjob_frontend/src/components/jobListing.js
+++ dreamjob_frontend/src/components/jobListing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 
 class JobListing extends Component {
   render(){
@@ -14,6 +15,12 @@ class JobListing extends Component {
             </li>
             <li>
               {this.props.job.status}
+            </li>
+            <li>
+              <div className="pull-left">
+                <Link to="/jobDetails"><button className='btn-primary'>Job Details</button></Link>
+              </div>
+              {/* {this.props.job.id} */}
             </li>
           </ul>
         </div>
