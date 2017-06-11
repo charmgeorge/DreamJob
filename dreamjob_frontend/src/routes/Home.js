69ded1379b8cc1b075f925b25735ea19e5f8807e
antoniooo <antoniooo@localhost.localdomain>
2017-06-11 15:28:49 -0700

    added styling to grid

--- dreamjob_frontend/src/routes/Home.js
+++ dreamjob_frontend/src/routes/Home.js
@@ -13,32 +13,33 @@ class Home extends Component {
         </Jumbotron>
 
         <br />
-
-        <Grid>
-            <Row className="show-grid">
-              <Col sm={6} md={3}>
-                <img className='thumbs' src="http://www.addspacetoyourlife.com/wp-content/uploads/2013/10/things-organized-neatly-clipboard.jpg" />
-                <br />
-                <h5>Stay Organized</h5>
-              </Col>
-              <Col sm={6} md={3}>
-                <img className='thumbs' src="http://www.radius180.com/wp-content/uploads/2014/04/save-time-money.jpg" />
-                <br />
-                <h5>Save Time</h5>
-              </Col>
-              <Clearfix visibleSmBlock><code>&lt;{'Clearfix visibleSmBlock'} /&gt;</code></Clearfix>
-              <Col sm={6} md={3}>
-                <img className='thumbs' src="http://blog.ss8.com/wp-content/uploads/2015/06/insider_threat.jpg" />
-                <br />
-                <h5>Get an inside peak at the company</h5>
-              </Col>
-              <Col sm={6} md={3}>
-                <img className='thumbs' src="https://thumbs.dreamstime.com/x/win-race-15103131.jpg" />
-                <br />
-                <h5>Get the job</h5>
-              </Col>
-            </Row>
-          </Grid>
+        <div className='container benefits'>
+          <Grid>
+              <Row className="show-grid">
+                <Col sm={6} md={3}>
+                  <img className='thumbs' src="http://www.addspacetoyourlife.com/wp-content/uploads/2013/10/things-organized-neatly-clipboard.jpg" />
+                  <br />
+                  <h5>Stay Organized</h5>
+                </Col>
+                <Col sm={6} md={3}>
+                  <img className='thumbs' src="http://www.radius180.com/wp-content/uploads/2014/04/save-time-money.jpg" />
+                  <br />
+                  <h5>Save Time</h5>
+                </Col>
+                <Clearfix visibleSmBlock><code>&lt;{'Clearfix visibleSmBlock'} /&gt;</code></Clearfix>
+                <Col sm={6} md={3}>
+                  <img className='thumbs' src="http://blog.ss8.com/wp-content/uploads/2015/06/insider_threat.jpg" />
+                  <br />
+                  <h5>Get an inside peak at the company</h5>
+                </Col>
+                <Col sm={6} md={3}>
+                  <img className='thumbs' src="https://thumbs.dreamstime.com/x/win-race-15103131.jpg" />
+                  <br />
+                  <h5>Get the job</h5>
+                </Col>
+              </Row>
+            </Grid>
+          </div>
       </div>
     );
   }
