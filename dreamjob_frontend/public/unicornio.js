d6522f1bf55f64a469415c59017acb57533ad705
learn <nick.bouldien@gmail.com>
2017-07-23 17:56:54 -0700

    removed console log

--- dreamjob_frontend/public/unicornio.js
+++ dreamjob_frontend/public/unicornio.js
@@ -1,7 +1,7 @@
 const pressed = [];
 const secretCode = 'bangfalseisawesome';
 window.addEventListener('keyup', (e) => {
-  console.log(e.key);
+  // console.log(e.key);
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   if (pressed.join('').includes(secretCode)) {
