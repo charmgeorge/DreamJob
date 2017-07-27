e95a7120fb7f2b4a9bc1305fd3ef1440929f8a0a
nick <nick.bouldien@gmail.com>
2017-07-26 23:01:12 -0700

    work on footer, add github icon

--- dreamjob_frontend/src/components/Footer.js
+++ dreamjob_frontend/src/components/Footer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import {Navbar, Nav, NavItem, Image} from 'react-bootstrap';
 import {BrowserRouter as Redirect, Link } from 'react-router-dom';
 
 const style = {
     backgroundColor: "#F8F8F8",
     borderTop: "1px solid #E7E7E7",
-    textAlign: "center",
+    // textAlign: "center",
+    // padding: "1rem",
     position: "relative",
     left: "0",
     bottom: "0",
@@ -14,7 +15,7 @@ const style = {
 };
 
 const Footer = () =>  (
-  <div style={style}>
+  <footer style={style}>
     <Navbar collapseOnSelect>
       <Navbar.Header>
         <Navbar.Toggle />
@@ -22,14 +23,14 @@ const Footer = () =>  (
       <Navbar.Collapse>
         <Nav bsStyle="pills" >
           <Navbar.Text>
-            <Navbar.Link href='https://github.com/charmgeorge/DreamJob' target="_blank">Project GitHub Link</Navbar.Link>
+            <Navbar.Link href='https://github.com/charmgeorge/DreamJob' target="_blank"><Image src="GitHub-Mark-32px.png" /></Navbar.Link>
           </Navbar.Text>
           <NavItem eventKey={2}><Link to='/about'>About the Team</Link></NavItem>
           <NavItem eventKey={3}><Link to='/project'>About the Project</Link></NavItem>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  </div>
+  </footer>
 );
 
 
